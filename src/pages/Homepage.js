4aef713ef880ad960ee0188e51b5a2093b06ca7e
cantanert <cantanert@gmail.com>
2020-01-12 23:59:07 +0300

    Homepage UI changes

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,48 +1,87 @@
 import React from 'react';
-import {SafeAreaView, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {SafeAreaView, Text, StyleSheet, TouchableOpacity, ImageBackground, View} from 'react-native';
 import Icon from "react-native-vector-icons/FontAwesome5";
 import statics from "../statics/enumStatics";
 
+//parsed image url parts
+const unsplashRootUrl = statics.unsplashRootUrl;
+const unsplashImageParameters = statics.unsplashImageParameters;
 
 export default class Homepage extends React.Component {
     render() {
         return (
             <SafeAreaView style={styles.homepageWrapper}>
-                <TouchableOpacity onPress={()=> this.props.navigation.navigate("Articles")} style={styles.menuItemArticles}>
-                    <Icon name="book-reader" style={styles.menuText}/>
-                    <Text style={styles.menuText}>{statics.articles}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={()=> this.props.navigation.navigate("TarotCards")} style={styles.menuItemTarotCards}>
-                    <Icon name="bandcamp" style={styles.menuText}/>
-                    <Text style={styles.menuText}>{statics.tarotCards}</Text>
-                </TouchableOpacity>
+                <ImageBackground
+                    source={{uri: unsplashRootUrl + "1568817864737-638432504138" + unsplashImageParameters}}
+                    style={styles.imageBackgroundWrapper}
+                    imageStyle={styles.imageBackgroundImageStyles}>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate("Articles")}
+                                      style={styles.menuItemArticles}>
+                        <View style={styles.itemTextBadge}/>
+                        <Icon name="book-reader" style={styles.menuIcon}/>
+                        <Text style={styles.menuText}>{statics.articles}</Text>
+                    </TouchableOpacity>
+                </ImageBackground>
+                <ImageBackground
+                    source={{uri: unsplashRootUrl + "1506362802973-bd1717de901c" + unsplashImageParameters}}
+                    style={styles.imageBackgroundWrapper}
+                    imageStyle={styles.imageBackgroundImageStyles}>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate("TarotCards")}
+                                      style={styles.menuItemTarotCards}>
+                        <View style={styles.itemTextBadge}/>
+                        <Icon name="bandcamp" style={styles.menuIcon}/>
+                        <Text style={styles.menuText}>{statics.tarotCards}</Text>
+                    </TouchableOpacity>
+                </ImageBackground>
             </SafeAreaView>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    imageBackgroundWrapper:{
+        width: "100%",
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    imageBackgroundImageStyles: {
+        resizeMode: "cover"
+    },
     homepageWrapper: {
         flex: 1,
     },
     menuItemArticles: {
         flexDirection: "row",
-        flex:1,
+        flex: 1,
         width: "100%",
-        alignItems:"center",
-        justifyContent:"center",
-        backgroundColor: "lightgray"
+        alignItems: "center",
+        justifyContent: "center",
     },
     menuItemTarotCards: {
         flexDirection: "row",
-        flex:1,
+        flex: 1,
         width: "100%",
-        alignItems:"center",
-        justifyContent:"center",
-        backgroundColor: "whitesmoke"
+        alignItems: "center",
+        justifyContent: "center",
     },
     menuText: {
         fontSize: 30,
-        marginLeft:5
+        marginLeft: 5,
+        zIndex: 1,
+        color: "white",
+        paddingLeft: 10,
+        fontWeight: "900"
+    },
+    menuIcon:{
+        fontSize: 100,
+        color: "whitesmoke"
+    },
+    itemTextBadge: {
+        backgroundColor: "black",
+        opacity: 0.4,
+        height: "100%",
+        width: "100%",
+        position: "absolute",
     }
 });
